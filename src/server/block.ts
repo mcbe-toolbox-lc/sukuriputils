40fa54debc37acf933b9fe6b01652bc116ba2711
LuckedCoronet <lc45@lc-studios.net>
2025-11-10 12:37:33 +0900

    feat(server): add block utils

---
+++ src/server/block.ts
@@ -0,0 +1,23 @@
+import * as mc from "@minecraft/server";
+
+/**
+ * Runs a command at the location of a block.
+ * @param block - The block to run the command at.
+ * @param command - The command to run.
+ * @returns Result of the command execution.
+ */
+export const runCommandAtBlock = (block: mc.Block, command: string): mc.CommandResult => {
+	const location = `${block.x} ${block.y} ${block.z}` as const;
+	const finalCommand = `execute positioned ${location} run ${command}`;
+	return block.dimension.runCommand(finalCommand);
+};
+
+/**
+ * Destroys a block using the `setblock` command.
+ * @param block - The block to destroy.
+ */
+export const destroyBlock = (block: mc.Block): void => {
+	const location = `${block.x} ${block.y} ${block.z}` as const;
+	const finalCommand = `setblock ${location} air destroy`;
+	block.dimension.runCommand(finalCommand);
+};
