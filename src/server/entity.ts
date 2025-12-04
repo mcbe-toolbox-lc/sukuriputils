381aa449f99f00b9c44dbc8d27cbe6c4337a65f7
LuckedCoronet <lc45@lc-studios.net>
2025-12-04 14:33:53 +0900

    feat(server): add entity util `getEntityForwardLocation()`

--- src/server/entity.ts
+++ src/server/entity.ts
@@ -11,3 +11,20 @@ export const isEntityAlive = (entity: mc.Entity): boolean => {
 	const healthComponent = entity.getComponent("health");
 	return healthComponent !== undefined && healthComponent.currentValue > 0;
 };
+
+/**
+ * Retrieves a location in front of an entity's head, based on its view direction.
+ *
+ * @param entity - The entity.
+ * @returns A new Vector3 representing the location in front of the entity's head.
+ * @throws This function can throw errors.
+ */
+export const getEntityForwardLocation = (entity: mc.Entity): mc.Vector3 => {
+	const headLocation = entity.getHeadLocation();
+	const viewDirection = entity.getViewDirection();
+	return {
+		x: headLocation.x + viewDirection.x,
+		y: headLocation.y + viewDirection.y,
+		z: headLocation.z + viewDirection.z,
+	};
+};
