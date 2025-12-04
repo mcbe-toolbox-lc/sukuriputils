2d0bc79c3fdf48af52c31991557da891cc664dbb
LuckedCoronet <lc45@lc-studios.net>
2025-12-04 14:49:54 +0900

    feat(server): add block utils `getRelativeBlockFrom()`

--- src/server/block.ts
+++ src/server/block.ts
@@ -27,3 +27,33 @@ export const destroyBlock = (block: mc.Block): void => {
 	const finalCommand = `setblock ${location} air destroy`;
 	block.dimension.runCommand(finalCommand);
 };
+
+/**
+ * Gets a block relative to another block in the specified direction.
+ *
+ * @param origin - The starting block from which to calculate the relative position.
+ * @param direction - The cardinal direction to move.
+ * @param steps - The number of blocks to move in the specified direction. Defaults to 1.
+ * @returns The block at the calculated relative position, could be undefined.
+ * @throws This function can throw errors.
+ */
+export const getRelativeBlockFrom = (
+	origin: mc.Block,
+	direction: mc.Direction,
+	steps = 1,
+): mc.Block | undefined => {
+	switch (direction) {
+		case mc.Direction.Up:
+			return origin.above(steps);
+		case mc.Direction.Down:
+			return origin.below(steps);
+		case mc.Direction.North:
+			return origin.north(steps);
+		case mc.Direction.South:
+			return origin.south(steps);
+		case mc.Direction.West:
+			return origin.west(steps);
+		case mc.Direction.East:
+			return origin.east(steps);
+	}
+};
