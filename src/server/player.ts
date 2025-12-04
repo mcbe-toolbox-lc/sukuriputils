36f2219fda407df67231dcb89f503efe99663d8f
LuckedCoronet <lc45@lc-studios.net>
2025-12-04 14:27:29 +0900

    feat(server): add player util `addCameraShake()`

--- src/server/player.ts
+++ src/server/player.ts
@@ -9,3 +9,20 @@ export const isPlayerCreativeOrSpectator = (player: mc.Player): boolean => {
 	const gameMode = player.getGameMode();
 	return gameMode === mc.GameMode.Creative || gameMode === mc.GameMode.Spectator;
 };
+
+/**
+ * Adds a camera shake effect to a player.
+ * @param player - The player to add the camera shake to.
+ * @param intensity - Shake intensity.
+ * @param seconds - Shake duration.
+ * @param mode - Shake mode.
+ * @throws This function can throw errors.
+ */
+export const addCameraShake = (
+	player: mc.Player,
+	intensity: number,
+	seconds: number,
+	mode: "positional" | "rotational",
+): void => {
+	player.runCommand(`camerashake add @s ${intensity} ${seconds} ${mode}`);
+};
